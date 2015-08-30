0964997cf8b3b8940f44053343c6b6aa688c1046
williamsmichael <willia32@hotmail.com>
2015-08-30 11:28:31 -0700

    makes the game dynamic by setting a high and low of choice; adds comments

--- L3_P9.py
+++ L3_P9.py
@@ -1,15 +1,18 @@
 # In this problem, you'll create a program that guesses a secret number!
 # Note: your program should be using raw_input to obtain the user's input! Be sure to handle the case when the user's input is not one of h, l, or c.
 
-guess_int = 50
-low = 0
-high = 100
 prompt = "Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. "
 user_response = ""
 
-print("Please think of a number between 0 and 100!")
+# initializes the game
+low = 0
+high = 100
+guess_int = (low + high) / 2 # first guess by the computer
 
+# start print
+print("Please think of a number between {} and {}!".format(low, high))
 
+# enter game, exits on keypress 'c' for correct answer
 while user_response != 'c':
   secret_guess = 'Is your secret number {}?'.format(guess_int)
   print(secret_guess)
@@ -18,22 +21,21 @@ while user_response != 'c':
   if user_response not in ('h', 'l', 'c'):
     print("Sorry, I did not understand your input.")
   else:
-    if user_response == 'h':
+    if user_response == 'h': # too high
       high = guess_int
       # print(low, " low")
       # print(high, " high")
 
-    elif user_response == 'l':
+    elif user_response == 'l': # too low
       low = guess_int
       # print(low, " low")
       # print(high, " high")
+
+  # reset the guess)int based on the new high or low
   guess_int = (high + low) / 2
 
 print("Game over. Your secret number was: {}".format(guess_int))
 
-
-
-# if __name__ == "__main__":
 
 
 
