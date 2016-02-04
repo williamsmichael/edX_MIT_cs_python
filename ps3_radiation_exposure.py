b068a22d8ea7158f5b58d8443f2f1f35e9683ca8
Michael Williams <willia32@hotmail.com>
2016-02-04 18:14:11 +0000

    Completes Problem Set 3.

--- ps3_radiation_exposure.py
+++ ps3_radiation_exposure.py
@@ -1,24 +1,31 @@
-# RADIATION EXPOSURE
-def radiationExposure(start, stop, step):
-    '''
-    Computes and returns the amount of radiation exposed
-    to between the start and stop times. Calls the 
-    function f (defined for you in the grading script)
-    to obtain the value of the function at any point.
- 
-    start: integer, the time at which exposure begins
-    stop: integer, the time at which exposure ends
-    step: float, the width of each rectangle. You can assume that
-      the step size will always partition the space evenly.
+# RADIATION EXPOSURE (25 points)
+def f(x):
+  import math
+  return 10 * math.e**(math.log(0.5) / 5.27 * x)
 
-    returns: float, the amount of radiation exposed to 
-      between start and stop times.
-    '''
-    def f(x):
-      import math
-      return 10 * math.e**(math.log(0.5) / 5.27 * x)
+def radiationExposure(start, stop, step):
+  '''
+  Computes and returns the amount of radiation exposed
+  to between the start and stop times. Calls the 
+  function f (defined for you in the grading script)
+  to obtain the value of the function at any point.
+  
+  start: integer, the time at which exposure begins
+  stop: integer, the time at which exposure ends
+  step: float, the width of each rectangle. You can assume that
+    the step size will always partition the space evenly.
+  
+  returns: float, the amount of radiation exposed to 
+    between start and stop times.
+  '''
+  calculated = 0
+  initial = start
+  while initial < stop:
+      calculated += f(initial) * step
+      initial += step
+  return calculated
       
 print radiationExposure(0, 5, 1) # return 39.10318784326239
-print radiationExposure(5, 11, 1) # return 22.94241041057671
-print radiationExposure(0, 11, 1) # return 62.0455982538
-print radiationExposure(40, 100, 1.5) # return 0.434612356115
+# print radiationExposure(5, 11, 1) # return 22.94241041057671
+# print radiationExposure(0, 11, 1) # return 62.0455982538
+# print radiationExposure(40, 100, 1.5) # return 0.434612356115
